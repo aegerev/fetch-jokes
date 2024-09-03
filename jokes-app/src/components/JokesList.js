fcc90186c8a365f9b826e2c014c08b43956df23d
alex <alexandersegerev@gmail.com>
2024-09-03 14:50:00 -0400

    Added the JokesList ID

---
+++ jokes-app/src/components/JokesList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import JokesItem from "./JokesItem";
+
+import "../App.css";
+
+
+function JokesList(props) {
+    const jokes = props.jokes.map((joke) => (
+    <JokesItem key={joke.id} text={joke.text} />
+    ));
+
+ return (
+    <ul className="jokesListWrapper">{jokes}</ul>;
+ );
+}
+
+export default JokesList;
